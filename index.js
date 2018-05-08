b728cffd8dc12d62cc7706959de6a4b805442d64
Igor Mucsicska <mucsi_96@yahoo.com>
2018-05-09 00:50:54 +0200

    getConfig

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-import { client } from 'nightwatch';
+const { cli, CliRunner } = require('nightwatch');
+const fs = require('fs');
+const path = require('path');
 
-console.log(client);
+
+function getConfig(env) {
+    const jsonConfigFile = './nightwatch.json'
+    const jsConfigFie = path.resolve('./nightwatch.conf.js');
+    const configFile = fs.existsSync(jsConfigFie) ? jsConfigFie : jsonConfigFile;
+    const runner = CliRunner({ config: configFile, env });
+    runner.setup();
+    return runner.settings;
+}
+
+console.log(getConfig('firefox'));
+
