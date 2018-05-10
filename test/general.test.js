5a5806f35d3ec39146de48ea90556a4b97d08c2e
Igor Mucsicska <mucsi_96@yahoo.com>
2018-05-10 15:27:22 +0200

    Assertion test added

--- test/general.test.js
+++ test/general.test.js
@@ -1,12 +1,30 @@
 const { client } = require('../src');
 
-describe('general', () => {
-  test('basic commands', async () => {
+describe('General features', () => {
+  test('Handles basic commands', async () => {
+    const onResultReady = jest.fn();
     await client
       .init()
       .setValue('#a', 4)
       .setValue('#b', 5)
       .click('#add')
-      .getText('#result', ({ value }) => expect(value).toEqual('9'));
+      .getText('#result', ({ value }) => onResultReady(parseInt(value)));
+    expect(onResultReady).toBeCalledWith(9);
+  });
+});
+
+describe('Assertion features', () => {
+  test('Handles assert.ok', async () => {
+    let sum;
+
+    await client
+      .init()
+      .setValue('#a', 4)
+      .setValue('#b', 5)
+      .click('#add')
+      .getText('#result', ({ value }) => {
+        sum = parseInt(value);
+      })
+      .assert.ok(sum === 9);
   });
 });
