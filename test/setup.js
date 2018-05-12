a4dffbebcb7043f8a29d0c650394671d5b94286e
Igor Mucsicska <mucsi_96@yahoo.com>
2018-05-12 20:47:22 +0200

    Fix paralell execution

--- test/setup.js
+++ test/setup.js
@@ -5,18 +5,21 @@ const portscanner = require('portscanner');
 jest.setTimeout(60000);
 
 const getFreePorts = async (startPort, endPort, n) => {
-  let port = startPort - 1;
+  let port = startPort;
   const ports = [];
   while (ports.length < n) {
     /* eslint-disable no-await-in-loop */
-    port = await portscanner.findAPortNotInUse(port + 1, 3050, '127.0.0.1');
+    port = await portscanner.findAPortNotInUse(port, endPort, '127.0.0.1');
     ports.push(port);
+    port += 1;
   }
   return ports;
 };
 
 beforeAll(async () => {
-  const [testAppPort, webDriverPort] = await getFreePorts(3000, 3050, 2);
+  const startPort = 3000 + (10 * process.env.JEST_WORKER_ID - 1);
+  const endPort = 3009 + (10 * process.env.JEST_WORKER_ID - 1);
+  const [testAppPort, webDriverPort] = await getFreePorts(startPort, endPort, 2);
   process.env.TEST_APP_PORT = testAppPort;
   process.env.WEB_DRIVER_PORT = webDriverPort;
   await startTestApp(testAppPort);
