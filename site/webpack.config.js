f7d707a056d59b51ba6b9fa4ab9e676d51f14b9d
Igor Mucsicska <mucsi_96@yahoo.com>
2018-07-04 00:26:02 +0200

    Improve site build

--- site/webpack.config.js
+++ site/webpack.config.js
@@ -1,43 +1,16 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 const StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
-
 const distPath = path.resolve(__dirname, '../site-dist');
-
-module.exports = {
-  mode: process.env.WEBPACK_SERVE ? 'development' : 'production',
-  entry: {
-    'site-server-renderer': path.resolve(__dirname, 'site-server-renderer.js'),
-    'site-client': path.resolve(__dirname, 'site-client.js')
-  },
+const commonConfig = {
   output: {
-    filename: '[name].js',
-    path: distPath,
-    libraryTarget: 'umd'
+    path: distPath
   },
   serve: {
     content: distPath,
     hot: false
   },
-  plugins: [
-    new StaticSiteGeneratorPlugin({
-      entry: 'site-server-renderer',
-      crawl: true,
-      globals: {
-        window: {}
-      },
-      locals: {
-        title: 'Nightwatch API',
-        description: 'Cucumber.js plugin for Nightwatch.js.',
-        url: 'http://mucsi96.github.io/nightwatch-cucumber/',
-        img: 'http://mucsi96.github.io/nightwatch-cucumber/res/img/opengraph.png',
-        githubId: 'mucsi96/nightwatch-api',
-        github: 'https://github.com/mucsi96/nightwatch-api',
-        npm: 'https://www.npmjs.com/package/nightwatch-api',
-        twitterId: 'mucsi96',
-        twitter: 'https://twitter.com/mucsi96'
-      }
-    })
-  ],
+  stats: 'minimal',
   module: {
     rules: [
       {
@@ -55,15 +28,52 @@ module.exports = {
         use: {
           loader: path.resolve(__dirname, 'markdown-loader.js')
         }
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: 'react-svg-loader'
-          }
-        ]
       }
     ]
   }
 };
+
+const serverConfig = {
+  ...commonConfig,
+  mode: 'development',
+  entry: path.resolve(__dirname, 'site-server-renderer.js'),
+  output: {
+    ...commonConfig.output,
+    libraryTarget: 'commonjs2',
+    filename: 'site-server-renderer.js'
+  },
+  target: 'node',
+  externals: nodeExternals(),
+  plugins: [
+    new StaticSiteGeneratorPlugin({
+      entry: 'main',
+      crawl: true,
+      globals: {
+        window: {}
+      },
+      locals: {
+        title: 'Nightwatch API',
+        description: 'Cucumber.js plugin for Nightwatch.js.',
+        url: 'http://mucsi96.github.io/nightwatch-cucumber/',
+        img: 'http://mucsi96.github.io/nightwatch-cucumber/res/img/opengraph.png',
+        githubId: 'mucsi96/nightwatch-api',
+        github: 'https://github.com/mucsi96/nightwatch-api',
+        npm: 'https://www.npmjs.com/package/nightwatch-api',
+        twitterId: 'mucsi96',
+        twitter: 'https://twitter.com/mucsi96'
+      }
+    })
+  ]
+};
+
+const clientConfig = {
+  ...commonConfig,
+  mode: process.env.WEBPACK_SERVE ? 'development' : 'production',
+  entry: path.resolve(__dirname, 'site-client.js'),
+  output: {
+    ...commonConfig.output,
+    filename: 'site-client.js'
+  }
+};
+
+module.exports = [clientConfig, serverConfig];
