62483721f34417d356d5d7054ce89220a5c45647
Igor Mucsicska <mucsi_96@yahoo.com>
2018-10-18 23:51:25 +0200

    Try to fix the build

--- site/components/InlineCode.js
+++ site/components/InlineCode.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const InlineCode = styled.code`
+const StyledInlineCode = styled.code`
   padding: 2px 4px;
   font-size: 90%;
   background-color: #f9f2f4;
   border-radius: 4px;
   font-family: Menlo, Monaco, Consolas, 'Courier New', monospace;
 `;
+
+const InlineCode = ({ children }) => <StyledInlineCode>{children}</StyledInlineCode>;
 
 export default InlineCode;
