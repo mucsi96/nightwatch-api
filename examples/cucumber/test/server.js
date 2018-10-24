ab6516e1b5c1a0fb9aa0c460bbd28e08911b2c54
Igor Mucsicska <mucsi_96@yahoo.com>
2018-10-24 22:26:03 +0200

    Try to fix the build

--- examples/cucumber/test/server.js
+++ examples/cucumber/test/server.js
@@ -1,3 +1,11 @@
-import { startWebDriver } from 'nightwatch-api';
+import { startWebDriver, stopWebDriver } from 'nightwatch-api';
 
 startWebDriver('default').catch(err => console.log(err));
+
+process.once('SIGINT', async () => {
+  try {
+    await stopWebDriver();
+  } finally {
+    process.exit();
+  }
+});
