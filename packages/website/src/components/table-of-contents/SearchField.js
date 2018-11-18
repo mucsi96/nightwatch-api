338d9a85d2ebead0ebc9a00ce38b5076b845bddf
Igor Mucsicska <mucsi_96@yahoo.com>
2018-11-18 01:40:18 +0100

    Add lighthouse audit

--- packages/website/src/components/table-of-contents/SearchField.js
+++ packages/website/src/components/table-of-contents/SearchField.js
@@ -8,43 +8,50 @@ const Wrapper = styled.div`
   .algolia-autocomplete {
     width: 100%;
   }
-`;
 
-const StyledIcon = styled(Icon)`
-  position: absolute;
-  top: 50%;
-  left: 3px;
-  transform: translateY(-50%);
-  width: 20px;
-  height: 20px;
-  z-index: 1;
-`;
+  .label {
+    position: absolute;
+    left: 0;
+    top: -500px;
+    width: 1px;
+    height: 1px;
+    overflow: hidden;
+  }
 
-const StyledInput = styled.input`
-  display: block;
-  width: 100%;
-  box-sizing: border-box;
-  background: white;
-  border: 0;
-  color: inherit;
-  font-size: 18px;
-  font-family: inherit;
-  position: relative;
-  padding: 5px 5px 5px 29px;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  outline-color: currentColor;
+  .icon {
+    position: absolute;
+    top: 50%;
+    left: 3px;
+    transform: translateY(-50%);
+    width: 20px;
+    height: 20px;
+    z-index: 1;
+  }
+
+  input {
+    display: block;
+    width: 100%;
+    box-sizing: border-box;
+    background: white;
+    border: 0;
+    color: inherit;
+    font-size: 18px;
+    font-family: inherit;
+    position: relative;
+    padding: 5px 5px 5px 29px;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    outline-color: currentColor;
+  }
 `;
 
 const SearchField = () => (
   <Wrapper>
-    <StyledIcon />
-    <StyledInput
-      type="search"
-      id="search-field"
-      placeholder="Search docs"
-      aria-label="Search docs"
-    />
+    <label for="search-field" class="label">
+      Search docs
+    </label>
+    <Icon className="icon" />
+    <input type="search" id="search-field" placeholder="Search docs" />
   </Wrapper>
 );
 
