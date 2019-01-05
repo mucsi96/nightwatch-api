31bceecff2a5e74e27832052950149dd7eb45fe7
Igor Mucsicska <mucsi_96@yahoo.com>
2019-01-05 18:08:10 +0100

    Update dependnecies. Closes #56

---
+++ packages/nightwatch-api/scripts/update-dependencies.ts
@@ -0,0 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+import { spawnSync } from 'child_process';
+
+const packages = fs.readdirSync(path.resolve(__dirname, '../..'));
+
+packages.forEach(packageName => {
+  const cwd = path.resolve(__dirname, '../..', packageName);
+  runCommand('npx npm-check-updates --upgradeAll', cwd);
+  runCommand('npm i', cwd);
+});
+
+function runCommand(command: string, cwd: string) {
+  console.log(`${cwd} > ${command}`);
+  spawnSync(command, { cwd, shell: true, stdio: 'inherit' });
+}
