6c02968ef6346b3f0ac1031992f353f0e7738b5c
Igor Mucsicska <mucsi_96@yahoo.com>
2019-02-02 15:04:31 +0100

    Imporve md loader

--- packages/website/src/loaders/markdown-loader.js
+++ packages/website/src/loaders/markdown-loader.js
@@ -6,61 +6,91 @@ const emojiRegex = [
   '\ud83d[\udc00-\ude4f]',
   '\ud83d[\ude80-\udeff]'
 ].join('|');
+const mdImport = `@import '[^']+'`;
+const mdImportWithPath = /@import '([^']+)'/;
+const relativeImage = '!\\[alt-tag\\]\\(\\..*?\\)';
+const relativeImageWithPath = /!\[alt-tag\]\((\..*?)\)/;
 
-function processMarkdownModuleChunk(moduleParts, chunk) {
-  const importMatch = chunk.match(/@import '([^']+)'/);
-  const emojiMatch = chunk.match(new RegExp(emojiRegex));
+function processMarkdownImport(moduleParts, path) {
+  const defaultImportVariable = `md${moduleParts.imports.length + 1}`;
+  const emojiMapVariable = `emojiMap${moduleParts.imports.length + 1}`;
+  const importStatement = `import ${defaultImportVariable}, { emojiMap as ${emojiMapVariable} } from '${path}';`;
+  const textChunk = `${defaultImportVariable}.replace(/<--EMOJI-MAP--.*?--EMOJI-MAP-->/, '')`;
 
-  if (importMatch) {
-    const defaultImportVariable = `md${moduleParts.imports.length + 1}`;
-    const emojiMapVariable = `emojiMap${moduleParts.imports.length + 1}`;
-    const importStatement = `import ${defaultImportVariable}, { emojiMap as ${emojiMapVariable} } from '${
-      importMatch[1]
-    }';`;
+  return {
+    ...moduleParts,
+    imports: [...moduleParts.imports, importStatement],
+    textChunks: [...moduleParts.textChunks, textChunk],
+    importedEmojiMaps: [...moduleParts.importedEmojiMaps, emojiMapVariable]
+  };
+}
 
-    return {
-      ...moduleParts,
-      imports: [...moduleParts.imports, importStatement],
-      textChunks: [...moduleParts.textChunks, defaultImportVariable],
-      importedEmojiMaps: [...moduleParts.importedEmojiMaps, emojiMapVariable]
-    };
-  }
+function processEmoji(moduleParts, emoji) {
+  const codePoint = twemoji.convert.toCodePoint(emoji);
+  const importVariable = `emoji${moduleParts.imports.length + 1}`;
+  const svgPath = `raw-loader!twemoji/2/svg/${codePoint}.svg`;
+  const importStatement = `import ${importVariable} from '${svgPath}';`;
 
-  if (emojiMatch) {
-    const emoji = emojiMatch[0];
-    const codePoint = twemoji.convert.toCodePoint(emoji);
-    const importVariable = `emoji${moduleParts.imports.length + 1}`;
-    const svgPath = `raw-loader!twemoji/2/svg/${codePoint}.svg`;
-    const importStatement = `import ${importVariable} from '${svgPath}';`;
+  return {
+    ...moduleParts,
+    imports: [...moduleParts.imports, importStatement],
+    textChunks: [...moduleParts.textChunks, `'[${codePoint}](#emoji)'`],
+    emojiMap: { ...moduleParts.emojiMap, [codePoint]: importVariable }
+  };
+}
 
-    return {
-      ...moduleParts,
-      imports: [...moduleParts.imports, importStatement],
-      textChunks: [...moduleParts.textChunks, `'[${codePoint}](#emoji)'`],
-      emojiMap: { ...moduleParts.emojiMap, [codePoint]: importVariable }
-    };
-  }
+function processRelativeImage(moduleParts, path) {
+  const importVariable = `relativeImage${moduleParts.imports.length + 1}`;
+  const importStatement = `import ${importVariable} from '${path}';`;
 
+  return {
+    ...moduleParts,
+    imports: [...moduleParts.imports, importStatement],
+    textChunks: [...moduleParts.textChunks, `'![alt-tag](' + ${importVariable} + ')'`]
+  };
+}
+
+function processTextChunk(moduleParts, chunk) {
   return {
     ...moduleParts,
     textChunks: [...moduleParts.textChunks, JSON.stringify(chunk)]
   };
 }
 
-module.exports = function() {
-  const content = fs.readFileSync(this.resourcePath, 'utf8');
+function processMarkdownChunk(moduleParts, chunk) {
+  const importMatch = chunk.match(mdImportWithPath);
+  const emojiMatch = chunk.match(new RegExp(emojiRegex));
+  const relativeImageMatch = chunk.match(relativeImageWithPath);
 
-  const { imports, textChunks, emojiMap, importedEmojiMaps } = content
-    .split(new RegExp(`(@import '[^']+'|${emojiRegex})`))
-    .filter(chunk => /\S/.test(chunk))
-    .reduce(processMarkdownModuleChunk, {
+  if (importMatch) {
+    return processMarkdownImport(moduleParts, importMatch[1]);
+  }
+
+  if (emojiMatch) {
+    return processEmoji(moduleParts, emojiMatch[0]);
+  }
+
+  if (relativeImageMatch) {
+    return processRelativeImage(moduleParts, relativeImageMatch[1]);
+  }
+
+  return processTextChunk(moduleParts, chunk);
+}
+
+function createESModulePartsFromMarkdown(markdownSource) {
+  return markdownSource
+    .split(new RegExp(`(${mdImport}|${emojiRegex}|${relativeImage})`))
+    .filter(chunk => chunk)
+    .reduce(processMarkdownChunk, {
       imports: [],
       textChunks: [],
       emojiMap: {},
       importedEmojiMaps: []
     });
+}
 
-  const moduleSource = [
+function createESModuleSourceFromParts({ imports, textChunks, emojiMap, importedEmojiMaps }) {
+  return [
     imports.join('\n'),
     `export const emojiMap = {\n${importedEmojiMaps
       .map(emojiMap => `...${emojiMap}`)
@@ -76,8 +106,14 @@ module.exports = function() {
   ]
     .filter(part => part.length)
     .join('\n');
+}
 
-  // process.stdout.write(moduleSource);
+module.exports = function() {
+  const markdownSource = fs.readFileSync(this.resourcePath, 'utf8');
 
-  return moduleSource;
+  const ESModuleSource = createESModuleSourceFromParts(
+    createESModulePartsFromMarkdown(markdownSource)
+  );
+
+  return ESModuleSource;
 };
