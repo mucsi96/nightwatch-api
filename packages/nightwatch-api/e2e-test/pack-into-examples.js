e8b6afe07e2951c2ae2f1235bdb1c7c17d29e919
Igor Mucsicska <mucsi_96@yahoo.com>
2019-02-10 18:21:21 +0100

    Run hub example on current version

--- packages/nightwatch-api/e2e-test/pack-into-examples.js
+++ packages/nightwatch-api/e2e-test/pack-into-examples.js
@@ -13,6 +13,9 @@ execSync(`npm i ../nightwatch-api/${tarball} && npm i`, {
 execSync(`npm i ../nightwatch-api/${tarball} && npm i`, {
   cwd: path.resolve(__dirname, '../../cucumber-selenium-example')
 });
+execSync(`cp ../nightwatch-api/${tarball} . && npm i ${tarball}`, {
+  cwd: path.resolve(__dirname, '../../cucumber-selenium-example')
+});
 execSync(`npm i ../nightwatch-api/${tarball} && npm i`, {
   cwd: path.resolve(__dirname, '../../node-example')
 });
