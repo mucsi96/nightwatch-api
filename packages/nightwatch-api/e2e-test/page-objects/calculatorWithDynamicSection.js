84082370cc7e9ff7b2588ca51bd2cb857eb8878c
spnraju <padmanabharaju.sagi@gmail.com>
2019-02-24 16:54:23 +0530

    fix session id issue in e2e tests

--- packages/nightwatch-api/e2e-test/page-objects/calculatorWithDynamicSection.js
+++ packages/nightwatch-api/e2e-test/page-objects/calculatorWithDynamicSection.js
@@ -17,7 +17,7 @@ const commands = {
               return this.click('#add');
             },
             checkResult: function() {
-              return this.assert.containsText('#result-value', expectedResult);
+              return this.api.assert.containsText('#result-value', expectedResult);
             }
           }
         ]
