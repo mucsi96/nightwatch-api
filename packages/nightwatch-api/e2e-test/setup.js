e9ff15d2b0bdb7d0fd22b718e0e99ee282b579fa
Igor Mucsicska <mucsi_96@yahoo.com>
2019-03-13 07:31:07 +0100

    Fix tests

--- packages/nightwatch-api/e2e-test/setup.js
+++ packages/nightwatch-api/e2e-test/setup.js
@@ -13,9 +13,3 @@ beforeAll(async () => {
 afterAll(async () => {
   await closeSession();
 });
-
-// For async tests, catch all errors here so we don't have to try / catch
-// everywhere for safety
-process.on('unhandledRejection', error => {
-  console.log(error);
-});
