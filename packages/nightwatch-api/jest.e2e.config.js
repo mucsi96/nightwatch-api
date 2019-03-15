a08b2112c935b1b6f892f3e75a9507b666cd04cf
Igor Mucsicska <mucsi_96@yahoo.com>
2019-03-15 10:34:50 +0100

    Fix coverage

--- packages/nightwatch-api/jest.e2e.config.js
+++ packages/nightwatch-api/jest.e2e.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   name: 'e2e',
   displayName: 'e2e',
-  collectCoverageFrom: ['src/**/*.{js,ts}'],
   testMatch: ['<rootDir>/e2e-test/**/*.test.js'],
   globalSetup: './e2e-test/global-setup.ts',
   globalTeardown: './e2e-test/global-teardown.ts',
--- packages/nightwatch-api/jest.unit.config.js
+++ packages/nightwatch-api/jest.unit.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   name: 'unit',
   displayName: 'unit',
-  collectCoverageFrom: ['src/**/*.{js,ts}'],
   testMatch: ['<rootDir>/src/**/*.test.js']
 };
