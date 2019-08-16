670b776d945f5dc12484030212923f7267a27b8a
Igor Muchychka <mucsi_96@yahoo.com>
2019-08-16 20:30:18 +0200

    Fix getAttribute test. Update the docs

--- packages/nightwatch-api/e2e-test/general.test.js
+++ packages/nightwatch-api/e2e-test/general.test.js
@@ -11,4 +11,31 @@ describe('General features', () => {
       .getText('#result-value', ({ value }) => onResultReady(parseInt(value)));
     expect(onResultReady).toBeCalledWith(9);
   });
+
+  test('Handles getAttribute success', async () => {
+    let id;
+    await client.init();
+    await client.getAttribute('#a', 'id', ({ value }) => {
+      id = value;
+    });
+    expect(id).toEqual('a');
+  });
+
+  test('Handles getAttribute failure', async () => {
+    await client.init();
+
+    let error;
+    try {
+      await client.getAttribute('#not-existing-element', 'test-attribute', ({ value }) => {
+        if (value.error) {
+          throw Error(value.error);
+        }
+      });
+    } catch (err) {
+      error = err;
+    }
+    expect(error.message).toMatch(
+      'An error occurred while running .getAttribute() command on <#not-existing-element>:'
+    );
+  });
 });
