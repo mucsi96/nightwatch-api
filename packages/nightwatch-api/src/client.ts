45294fbe9a3c38018627b3411d2ba5b39de173fd
Jim Reid <jreid@oncourselearning.com>
2019-09-30 16:03:20 -0500

    setting up runner to support browserstack-local package

--- packages/nightwatch-api/src/client.ts
+++ packages/nightwatch-api/src/client.ts
@@ -11,17 +11,26 @@ import fs from 'fs';
 import { log } from './logger';
 import { createFailureScreenshot } from './screenshots';
 import reporter from './reporter';
+import browserstack from 'browserstack-local';
 
 interface IOptions {
   env?: string;
   configFile?: string;
   silent?: boolean;
+  browserStackOptions?: IBrowserStackOptions;
+}
+
+interface IBrowserStackOptions {
+  enabled?: boolean;
+  useLocal?: boolean;
+  accessKey?: string;
 }
 
 let runner: CliRunnerInstance | null;
 let runnerOptions: IOptions | null;
 let client: Client | null;
 let screenshots: string[] = [];
+let browserStackLocal: browserstack.Local | null;
 
 export function deleteRunner() {
   runner = null;
@@ -55,33 +64,97 @@ function getDefaultConfigFile() {
   );
 }
 
-function createRunner(options: IOptions) {
-  if (!runner) {
+function getDefaultBrowserStackOptions() {
+  let options: IBrowserStackOptions = {
+    enabled: false,
+    useLocal: false,
+    accessKey: undefined
+  };
+  return options;
+}
+
+async function createRunner(options: IOptions) {
+  if (runner) {
+    return runner;
+  }
+
+  return new Promise<CliRunnerInstance | null>((resolve, reject) => {
     runnerOptions = {
       env: (options && options.env) || getDefaultEnvironment(),
       configFile: (options && options.configFile) || getDefaultConfigFile(),
-      silent: (options && options.silent) || false
+      silent: (options && options.silent) || false,
+      browserStackOptions:
+        (options && options.browserStackOptions) || getDefaultBrowserStackOptions()
     };
+
+    let browserStackEnabled: boolean | undefined;
+    let browserStackUseLocal: boolean | undefined;
+    let browserStackAccessKey: string | undefined;
+
+    if (runnerOptions.browserStackOptions) {
+      browserStackEnabled = runnerOptions.browserStackOptions.enabled;
+      browserStackUseLocal = runnerOptions.browserStackOptions.useLocal;
+      browserStackAccessKey = runnerOptions.browserStackOptions.accessKey;
+    }
+
     runner = CliRunner({ env: runnerOptions.env, config: runnerOptions.configFile });
     runner.isWebDriverManaged = function() {
+      const managed = typeof browserStackEnabled === 'undefined' ? true : !browserStackEnabled;
       if (this.baseSettings.selenium) {
-        this.baseSettings.selenium.start_process = true;
+        this.baseSettings.selenium.start_process = managed;
       }
-      return true;
+      return managed;
     };
-    runner.setup();
-  }
 
-  return runner;
+    if (browserStackEnabled && browserStackUseLocal) {
+      browserStackLocal = new browserstack.Local();
+      browserStackLocal.start({ key: browserStackAccessKey }, function() {
+        if (runner) {
+          runner.setup();
+          resolve(runner);
+        } else {
+          reject('Failed to create the runner within BrowserStack Local');
+        }
+      });
+    } else {
+      runner.setup();
+      resolve(runner);
+    }
+  });
+}
+
+async function stopBrowserStackLocal() {
+  return new Promise<void>((resolve, reject) => {
+    if (browserStackLocal) {
+      browserStackLocal.stop(function() {
+        log('BrowserStack Local stopped');
+        resolve();
+      });
+    } else {
+      resolve();
+    }
+  });
 }
 
 export async function startWebDriver(options: IOptions) {
   deleteRunner();
-  const runner = createRunner(options);
+  const runner = await createRunner(options);
 
-  const { port } = runner.test_settings.webdriver;
+  if (!runner) {
+    return Promise.reject("The CliRunner instance wasn't properly created");
+  }
+
+  let driverPort: number | undefined;
+  if (runner.test_settings.webdriver) {
+    const { port } = runner.test_settings.webdriver;
+    driverPort = port;
+  }
+
   await runner.startWebDriver();
-  log(`WebDriver started on port ${port} for ${runner.testEnv} environment`);
+
+  if (driverPort) {
+    log(`WebDriver started on port ${driverPort} for ${runner.testEnv} environment`);
+  }
 }
 
 export async function stopWebDriver() {
@@ -89,9 +162,19 @@ export async function stopWebDriver() {
     return;
   }
 
-  const { port } = runner.test_settings.webdriver;
+  await stopBrowserStackLocal();
+
+  let driverPort: number | undefined;
+  if (runner.test_settings.webdriver) {
+    const { port } = runner.test_settings.webdriver;
+    driverPort = port;
+  }
+
   await runner.stopWebDriver();
-  log(`WebDriver stopped on port ${port} for ${runner.testEnv} environment`);
+
+  if (driverPort) {
+    log(`WebDriver stopped on port ${driverPort} for ${runner.testEnv} environment`);
+  }
 }
 
 export async function createSession(options: IOptions): Promise<Api> {
@@ -99,13 +182,23 @@ export async function createSession(options: IOptions): Promise<Api> {
     deleteRunner();
   }
 
-  const runner = createRunner(options);
+  const runner = await createRunner(options);
+
+  if (!runner) {
+    return Promise.reject("The CliRunner instance wasn't properly created");
+  }
+
   const settings = runner.test_settings;
   client =
     runnerOptions && runnerOptions.silent
       ? createClient(settings)
       : createClient(settings, new reporter());
-  log(`Creating session for ${runner.testEnv} environment on port ${settings.webdriver.port}`);
+
+  if (settings.webdriver && settings.webdriver.port) {
+    log(`Creating session for ${runner.testEnv} environment on port ${settings.webdriver.port}`);
+  } else {
+    log(`Creating session for ${runner.testEnv} environment`);
+  }
   await client.startSession();
   log(`Session created for ${runner.testEnv} environment`);
   return client.api;
