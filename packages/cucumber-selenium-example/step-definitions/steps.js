be053d44382d92de202a647f80a025fc290eca4f
Igor Mucsicska <mucsi_96@yahoo.com>
2019-11-20 00:07:12 +0100

    Fix DuckDuckGo examples

--- packages/cucumber-selenium-example/step-definitions/steps.js
+++ packages/cucumber-selenium-example/step-definitions/steps.js
@@ -18,5 +18,5 @@ Then(/^the Google search form exists$/, async () => {
 });
 
 Then(/^the DuckDuckGo search form exists$/, async () => {
-  await client.assert.visible('input[name="q"]');
+  await client.assert.visible('#search_form_input_homepage');
 });
